936c0323441cc3d7cbe6312d6cd4b5dc5e6624b5
Sertac1911u <114613689+Sertac1911u@users.noreply.github.com>
2025-03-16 21:57:20 +0300

    updated

---
+++ Frontends/Ecommerce.WebUI/ViewComponents/ProductListViewComponents/_ProductListCategoriesFilterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.WebUI.ViewComponents.ProductListViewComponents
+{
+    public class _ProductListCategoriesFilterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
